60bc7f94550a4ace79def5347d7e764985f5ba9e
MrBurp <andreass.ziegler@gmail.com>
2022-10-23 17:13:17 +0200

    inside display style

--- Calculator-Svelte/src/components/display.svelte
+++ Calculator-Svelte/src/components/display.svelte
@@ -9,5 +9,17 @@
 <style>
   .displayD{
     display: flex;
+    flex: 1;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 0px 5px;
+    font-size: 1.7rem;
+    background-color: aliceblue;
+    overflow: hidden;
+    margin: 2px 1px;
+    border-top-right-radius: 2px;
+    border-top-left-radius: 2px;
+
+
   }
 </style>
