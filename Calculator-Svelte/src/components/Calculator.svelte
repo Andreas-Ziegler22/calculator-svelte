be8f1a2cf127b2575e18b91c0efff14810c6b0ab
MrBurp <andreass.ziegler@gmail.com>
2022-10-23 11:38:59 +0200

    Slot concept  line and button as a child

--- Calculator-Svelte/src/components/Calculator.svelte
+++ Calculator-Svelte/src/components/Calculator.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
  
   import Button from "./button.svelte"
+  import Line from "./Line.svelte";
 
 </script>
 
 
 
 <div class="calculator">
+  <Line>
   <Button text="7"/>
   <Button text="8"/>
   <Button text="9"/>
+</Line>
+<Line>
+  <Button text="4"/>
+  <Button text="5"/>
+  <Button text="6"/>
+</Line>
 </div>
 
 <style>
