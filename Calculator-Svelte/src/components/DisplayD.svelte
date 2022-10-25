5d1554c31b6095d2b9e7e9073678d64b0f58a9c6
MrBurp <andreass.ziegler@gmail.com>
2022-10-25 15:04:09 +0200

    redone display

--- Calculator-Svelte/src/components/DisplayD.svelte
+++ Calculator-Svelte/src/components/DisplayD.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   export let valueD: string
 
-  $: changeFontSize = valueD.length > 20 ? 'small' : `s-${valueD.length}`// <--- causing trouble in code looking forward to solve this
+ $: changeFontSize = valueD.length > 20 ? 'small' : `s-${valueD.length}`
 </script>
 
-<div class={`displayD ${changeFontSize}`}><span>{valueD}</span></div>
+<div class={`displayD ${changeFontSize}`}>
+  <span>{valueD}</span>
+</div>
 
 
 <style>
@@ -22,18 +24,8 @@
         border-top-left-radius: 5px;
     }
 
-    .s-10 { font-size: 1.5rem; }
-    .s-11 { font-size: 1.35rem; }
-    .s-12 { font-size: 1.25rem; }
-    .s-13 { font-size: 1.15rem; }
-    .s-14 { font-size: 1.07rem; }
-    .s-15 { font-size: 1.0rem; }
-    .s-16 { font-size: 0.93rem; }
-    .s-17 { font-size: 0.87rem; }
-    .s-18 { font-size: 0.83rem; }
-    .s-19 { font-size: 0.78rem; }
-    .s-20 { font-size: 0.74rem; }
-
-    .small { font-size: 0.6rem; }
-
+    .s-{font-size: 1.5rem;}
+    .s-{font-size: 1.35rem;}
+    
+    .small{font-size: 0.6rem;}
 </style>
