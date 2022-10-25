9d19b738b3efd8546ca5e418fccd74587b8ffc73
MrBurp <andreass.ziegler@gmail.com>
2022-10-25 09:41:25 +0200

    display component working

---
+++ Calculator-Svelte/src/components/Display.svelte
@@ -0,0 +1,25 @@
+<Script lang="ts">
+  export let valor: string
+</Script>
+
+
+<div class="display">
+  <span>{valor}</span>
+</div>
+
+<style>
+  .display{
+    display: flex;
+    flex: 1;
+    background-color: aliceblue;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 0px 5px;
+    overflow: hidden;
+    font-size: 1.7rem;
+    margin: 2px 1px;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+  }
+
+</style>
